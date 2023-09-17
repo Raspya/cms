399d281ef5715ecfbb0c39420fb693f69a0f66d5
Donovan Lefevre <lfv.donovan@gmail.com>
2023-09-17 18:11:58 +0200

    Ajout Version

---
+++ src/main/java/dev/bernouy/cms/common/FileSystem.java
@@ -0,0 +1,8 @@
+package dev.bernouy.cms.common;
+
+import java.io.File;
+
+public class FileSystem {
+
+    public final static String COMPONENT_PATH = FileSystem.class.getResource("/component").getFile().toString();
+}
