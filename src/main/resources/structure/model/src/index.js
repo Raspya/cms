f34409dceed26dd320e242b9f188e61fa71f514f
Matthias Bernouy <matt.bernouy@orange.fr>
2023-11-02 13:23:44 +0100

    Add Structure Files

---
+++ src/main/resources/structure/model/src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client';
+import {Route, Routes} from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello World</h1>);
